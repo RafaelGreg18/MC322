a24c595814bcdc2fec5e196eb133c1b0cae7709f
Rafael Gregori <92278981+RafaelGreg18@users.noreply.github.com>
2022-04-07 13:40:57 -0300

    Delete AppLombriga2.java

--- lab03/src/pt/c02oo/s02classe/s03lombriga/AppLombriga2.java
+++
@@ -1,9 +0,0 @@
-public class AppLab03{
-    public static void main(String[] args){
-        Animacao anim = new Animacao("010101MCMMV");
-        for(int i = 0; i < 4; i++){
-            anim.passo();
-            anim.apresenta();
-        }
-    }
-}
